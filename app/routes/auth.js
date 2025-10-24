9cdfafc28bdbae585ddfdbb44b1b9c8e6331a546
CRW405 <134819932+CRW405@users.noreply.github.com>
2025-10-24 14:26:05 -0500

    created the routes and logic required to auth with spotify and get a users info back among other smaller fixes and tweaks

---
+++ app/routes/auth.js
@@ -0,0 +1,66 @@
+const { stringify } = require("querystring");
+const express = require("express")
+const app = express.Router()
+
+// Login with Spotify Account
+// this runs when user clicks sign in with spotify and redirects them to the spotify authorization page
+app.get("/login", (req, res) => {
+  // set scope of what our app can do
+  const scope = ["user-read-private", "user-read-email"]
+  // send user to auth page with our app credentials
+  res.redirect("https://accounts.spotify.com/authorize?" + stringify({
+    response_type: "code",
+    client_id: process.env.ID,
+    scope: scope,
+    redirect_uri: process.env.REDIRECT
+  }))
+})
+
+// this is where the user is sent after authorizing our app 
+// and it requests a token from spotify so that we can use the api in the name of the user
+app.get("/callback", async (req, res) => {
+  // gets the code from the url, if there is no code available, set it to null
+  // TODO: redirect the user back to auth if no token
+  const code = req.query.code || null;
+  // this builds the token request
+  const authOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Authorization": "Basic " + Buffer.from(process.env.ID + ":" + process.env.SECRET).toString("base64")
+    },
+    body: new URLSearchParams({
+      code: code,
+      redirect_uri: process.env.REDIRECT,
+      grant_type: "authorization_code"
+    })
+  };
+
+  // send the request and wait for it to return
+  const tokenResponse = await fetch("https://accounts.spotify.com/api/token", authOptions);
+  // read the token as json
+  const tokenData = await tokenResponse.json();
+  // extract token
+  const accessToken = tokenData.access_token;
+
+  // use token to get user data
+  const userData = await getProfile(accessToken);
+
+  // TODO: redirect to the user page
+  res.send({ user: userData });
+});
+
+async function getProfile(accessToken) {
+  // send user data request with token
+  const response = await fetch("https://api.spotify.com/v1/me", {
+    headers: {
+      Authorization: "Bearer " + accessToken
+    }
+  })
+
+  // wait for it to come in and return it from the function
+  const data = await response.json()
+  return data
+}
+
+module.exports = app
