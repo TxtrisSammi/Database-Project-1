6b5a187add438f15a553dd910d7f9b72a7b924f5
CRW405 <134819932+CRW405@users.noreply.github.com>
2025-10-29 10:14:46 -0500

    rm router.js and updated ejs anchors to use correct routes and rm redundant views

--- app/app.js
+++ app/app.js
@@ -3,7 +3,6 @@ require("dotenv").config(); // allow reading from .env
 const express = require('express');
 const path = require('path');
 const session = require("express-session")
-const router = require('./router')
 
 // create app
 const app = express();
@@ -29,11 +28,6 @@ const userRoutes = require("./routes/user");
 app.get('/', (req, res) => {
   res.render("index.ejs")
 })
-
-
-// routing stuff 
-app.use('/', router);
-
 
 // import routes
 app.use("/", authRoutes)
